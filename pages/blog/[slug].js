52da81eab8d341fb417f13dd40fce2e82190ec10
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-09 18:42:25 +0100

    first commit

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -7,7 +7,7 @@ import Layout from '@/components/Layout'
 import CategoryLabel from '@/components/CategoryLabel'
 
 export default function PostPage({
-  frontmatter: { title, category, date, cover_image, author, author_image },
+  frontmatter: { title, title2, source1, source2, category, date, cover_image, author, author_image },
   content,
   slug,
 }) {
@@ -16,19 +16,18 @@ export default function PostPage({
       <Link href='/blog'>Go Back</Link>
       <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
         <div className='flex justify-between items-center mt-4'>
-          <h1 className='text-5xl mb-7'>{title}</h1>
-          <CategoryLabel>{category}</CategoryLabel>
+          <h1 className='text-4xl '>{source1} : {title}</h1>
+          {/* <CategoryLabel>{category}</CategoryLabel> */}
         </div>
-        <img src={cover_image} alt='' className='w-full rounded' />
+        <div className='flex justify-between items-center mt-4'>
+          <h1 className='text-4xl mb-7'>{source2} : {title2}</h1>
+        </div>
+
+        <img src={cover_image} alt='' className=' w-auto rounded' />
 
         <div className='flex justify-between items-center bg-gray-100 p-2 my-8'>
           <div className='flex items-center'>
-            <img
-              src={author_image}
-              alt=''
-              className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block'
-            />
-            <h4>{author}</h4>
+
           </div>
           <div className='mr-4'>{date}</div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,11 @@ import { getPosts } from '@/lib/posts'
 export default function HomePage({ posts }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
+      <h1 className='text-5xl pb-5 font-bold'>Latest News</h1>
 
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5'>
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
@@ -16,7 +18,7 @@ export default function HomePage({ posts }) {
 
       <Link href='/blog'>
         <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
-          All Posts
+          All News
         </a>
       </Link>
     </Layout>
